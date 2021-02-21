d609c7c095493df42fe260ac79d436877a8f747f
LapitskayaN <lapitskaya.nl@gmail.com>
2021-02-21 18:50:13 +0300

    try add comments

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import TextField  from '@material-ui/core/TextField';
+// import { Button } from '@material-ui/core';
 
 
 class Review extends React.Component {
   constructor() {
     super()
-    this.state = {
-        text: '',
-        comment: []
-    }
+  //   this.state = {
+  //       text: '',
+  //       comment: []
+  //   }
   }
+
+  // handleClick = (comment) => {
+  //   this.setState({
+  //     comments: [...this.state.comments, comment]
+  //   })
+  // }
+
 
 
   render() {
-        const { text, comment } = this.state
-        // console.log(text)
+        // const { text, comment } = this.state
+       
         return (
+        <div className="reviewSection">
           <TextField  className='review-field' label="Review" variant="outlined" />
+          
+          {/* <Button variant="contained" color="secondary" 
+              onClick={()=>this.handleClick(text)}>
+                Send
+          </Button> 
 
+          <div className="comments-block">
+            {comments ?.map((comment) => {
+                return <p className="comment">{comment}</p>
+              })}
+          </div>
+         */}
+        
+        </div>
         )
 
   } 
